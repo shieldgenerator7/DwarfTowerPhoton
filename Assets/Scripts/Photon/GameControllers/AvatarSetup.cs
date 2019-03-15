957aa5825c32e4fd4e53e8ab870c91ebf4ce2c77
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-03-15 16:22:44 -0400

    Added CameraController

--- Assets/Scripts/Photon/GameControllers/AvatarSetup.cs
+++ Assets/Scripts/Photon/GameControllers/AvatarSetup.cs
@@ -17,6 +17,7 @@ public class AvatarSetup : MonoBehaviour
         if (PV.IsMine)
         {
             PV.RPC("RPC_AddCharacter", RpcTarget.AllBuffered, PlayerInfo.instance.mySelectedCharacter);
+            Camera.main.GetComponent<CameraController>().FocusObject = gameObject;
         }
     }
 
---
+++ Assets/Scripts/Photon/GameControllers/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject focusObject;//object the camera follows, usually the player
+    private Vector3 offset;
+    public GameObject FocusObject
+    {
+        get { return focusObject; }
+        set
+        {
+            focusObject = value;
+            float zOffset = transform.position.z - focusObject.transform.position.z;
+            offset = new Vector3(0, 0, zOffset);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (focusObject != null)
+        {
+            FocusObject = focusObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (focusObject != null)
+        {
+            transform.position = focusObject.transform.position + offset;
+        }
+    }
+}
