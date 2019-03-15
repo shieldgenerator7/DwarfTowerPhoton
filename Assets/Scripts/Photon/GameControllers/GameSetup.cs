458e71dc14c68e773e18f9d18d689427f0570d86
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-03-15 13:39:08 -0400

    Players now spawn in random locations (from tutorial)
    To do this, I followed steps from this tutorial:
    https://www.youtube.com/watch?v=JjfPaY57dDM

---
+++ Assets/Scripts/Photon/GameControllers/GameSetup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{//2019-03-15: made by following this tutorial: https://www.youtube.com/watch?v=JjfPaY57dDM
+
+    public static GameSetup instance;
+
+    public Transform[] spawnPoints;
+
+    private void OnEnable()
+    {
+        if (GameSetup.instance == null)
+        {
+            GameSetup.instance = this;
+        }
+    }
+}
---
+++ Assets/Scripts/Photon/GameControllers/PhotonPlayer.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PhotonPlayer : MonoBehaviour
+{//2019-03-15: made by following this tutorial: https://www.youtube.com/watch?v=JjfPaY57dDM
+
+    private PhotonView PV;
+    public GameObject myAvatar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        if (PV.IsMine)
+        {
+            int spawnPicker = Random.Range(0, GameSetup.instance.spawnPoints.Length);
+            myAvatar = PhotonNetwork.Instantiate(
+                Path.Combine("PhotonPrefabs", "PlayerAvatar"),
+                GameSetup.instance.spawnPoints[spawnPicker].position,
+                GameSetup.instance.spawnPoints[spawnPicker].rotation,
+                0
+                );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
