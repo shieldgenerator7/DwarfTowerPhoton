f8d42f13b95b43fc81ce8ad0361d9316db94c81d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-03-24 23:01:06 -0400

    Added ability context switcher

--- Assets/Characters/AbilityContext.cs
+++ Assets/Characters/AbilityContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class AbilityContext : MonoBehaviour
 {
     public List<PlayerAbility> abilities = new List<PlayerAbility>();
---
+++ Assets/Characters/_Abilities/AbilityContextSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityContextSwitcher : PlayerAbility
+{
+    public AbilityContext abilityContext;
+
+    public override void OnButtonUp()
+    {
+        base.OnButtonUp();
+        playerController.AbilityContext = abilityContext;
+    }
+}
