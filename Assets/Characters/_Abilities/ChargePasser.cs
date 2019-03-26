9bb9ad8acf45a3c0a76744223966f13e6d0e6552
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-03-25 23:48:30 -0400

    ChargePasser no longer shadows Start()

--- Assets/Characters/_Abilities/ChargePasser.cs
+++ Assets/Characters/_Abilities/ChargePasser.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ChargePasser : ChargedShotController
 {
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
         GetComponent<ObjectSpawner>().onObjectSpawned += passCharge;
     }
