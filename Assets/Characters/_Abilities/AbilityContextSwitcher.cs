74ae2e23d1b77a8e5e55d742b700dc157fb000f8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-03-26 02:37:20 -0400

    Astral switches back to default control scheme after making a weapon

--- Assets/Characters/_Abilities/AbilityContextSwitcher.cs
+++ Assets/Characters/_Abilities/AbilityContextSwitcher.cs
@@ -6,9 +6,37 @@ public class AbilityContextSwitcher : PlayerAbility
 {
     public AbilityContext abilityContext;
 
+    public List<PlayerAbility> switchOnAbilities;
+
+    protected override void Start()
+    {
+        base.Start();
+        foreach(PlayerAbility pa in switchOnAbilities)
+        {
+            if (pa is GunController)
+            {
+                ((GunController)pa).onShotFired += switchContextOnShot;
+            }
+            if (pa is ChargedGunController)
+            {
+                ((ChargedGunController)pa).onShotFired += switchContextOnShot;
+            }
+        }
+    }
+
     public override void OnButtonUp()
     {
         base.OnButtonUp();
+        switchContext();
+    }
+
+    public void switchContext()
+    {
         playerController.AbilityContext = abilityContext;
+    }
+
+    public void switchContextOnShot(GameObject shot, Vector2 pos, Vector2 dir)
+    {
+        switchContext();
     }
 }
