315cd2cd2063c85ece82790ac774d44e18b227d0
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-03-31 23:10:08 -0400

    Added Utility.teleportObject()

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -10,4 +10,17 @@ public static class Utility
     {
         get { return Camera.main.ScreenToWorldPoint(Input.mousePosition); }
     }
+
+    public static IEnumerator teleportObject(GameObject go, Vector3 newPosition)
+    {
+        Rigidbody2D rb2d = go.GetComponent<Rigidbody2D>();
+        rb2d.isKinematic = true;
+        while (go.transform.position != newPosition)
+        {
+            go.transform.position = newPosition;
+            yield return null;
+        }
+        rb2d.isKinematic = false;
+        yield return null;
+    }
 }
