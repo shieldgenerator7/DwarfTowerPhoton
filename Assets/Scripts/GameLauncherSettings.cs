27d6197a51724d308c09bc2f828eae8266a88530
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-04-01 01:52:30 -0400

    Hopefully made killing lunached clients more consistent (other half committed in last commit)
    The changes that went along with this is CustomMenu.cs got committed in the previous commit (25dd16ca4db757b59cda67518ab4c4bfa3eaf46d)

--- Assets/Scripts/GameLauncherSettings.cs
+++ Assets/Scripts/GameLauncherSettings.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class GameLauncherSettings : MonoBehaviour
 {
-    [Range(1,10)]
+    [Range(1, 10)]
     public int clientCount = 4;
     public bool resetClients = true;
+
+    public List<Process> buildProcesses = new List<Process>();
 }
