abaa8f548937d304efe9461c1002c7f2ccc17097
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-20 07:40:27 -0500

    [UNICORN] Make Rainbow Paths expire

--- Assets/Characters/Unicorn/Abilities/RainbowPathController.cs
+++ Assets/Characters/Unicorn/Abilities/RainbowPathController.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,27 @@ public class RainbowPathController : MonoBehaviour
 {
     public float speedMultiplier = 1.5f;
     public float slowMultiplier = 0.75f;
+    public float duration = 10;
+
+    private float startTime;
+    private PhotonView PV;
+
+    private void Start()
+    {
+        startTime = Time.time;
+        PV = GetComponent<PhotonView>();
+    }
+
+    private void Update()
+    {
+        if (PV.IsMine)
+        {
+            if (Time.time - startTime >= duration)
+            {
+                PhotonNetwork.Destroy(this.gameObject);
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
