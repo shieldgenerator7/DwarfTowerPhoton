c020b60dbe75bfe2c39e0b9a0d0fe433921dd13f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-20 00:28:05 -0500

    Add tooltips to two scriptable object types

--- Assets/Characters/Astral/Abilities/WeaponControllerData.cs
+++ Assets/Characters/Astral/Abilities/WeaponControllerData.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Characters/Astral/WeaponControllerData", order = 1)]
 public class WeaponControllerData : ScriptableObject
 {
+    [Tooltip("The angle between the facing direction and the weapon's hold direction")]
     public float positionAngle = 0;//the angle between the facing direction and the weapon's hold direction
+    [Tooltip("The angle between the hold direction and the weapon's rotation")]
     public float rotationAngle = 0;//the angle between the hold direction and the weapon's rotation
+    [Tooltip("The distance between the weapon and the holder")]
     public float holdBuffer = 1;
 }
