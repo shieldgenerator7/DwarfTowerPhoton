163395d34403baa231906502979e98ff7bacf4cc
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-21 06:30:43 -0500

    Add more tools to CustomEditor

--- Assets/Editor/CustomMenu.cs
+++ Assets/Editor/CustomMenu.cs
@@ -8,6 +8,60 @@ using Debug = UnityEngine.Debug;
 
 public class CustomMenu
 {
+    //Find Missing Scripts
+    //2018-04-13: copied from http://wiki.unity3d.com/index.php?title=FindMissingScripts
+    static int go_count = 0, components_count = 0, missing_count = 0;
+    [MenuItem("SG7/Editor/Refactor/Find Missing Scripts")]
+    private static void FindMissingScripts()
+    {
+        go_count = 0;
+        components_count = 0;
+        missing_count = 0;
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene s = SceneManager.GetSceneAt(i);
+            if (s.isLoaded)
+            {
+                foreach (GameObject go in s.GetRootGameObjects())
+                {
+                    FindInGO(go);
+                }
+            }
+        }
+        Debug.Log(string.Format("Searched {0} GameObjects, {1} components, found {2} missing", go_count, components_count, missing_count));
+    }
+    private static void FindInGO(GameObject g)
+    {
+        go_count++;
+        Component[] components = g.GetComponents<Component>();
+        for (int i = 0; i < components.Length; i++)
+        {
+            components_count++;
+            if (components[i] == null)
+            {
+                missing_count++;
+                string s = g.name;
+                Transform t = g.transform;
+                while (t.parent != null)
+                {
+                    s = t.parent.name + "/" + s;
+                    t = t.parent;
+                }
+                Debug.Log(s + " has an empty script attached in position: " + i, g);
+            }
+        }
+        // Now recurse through each child GO (if there are any):
+        foreach (Transform childT in g.transform)
+        {
+            FindInGO(childT.gameObject);
+        }
+    }
+
+    [MenuItem("SG7/Editor/Show or Hide All Colliders %&c")]
+    public static void showHideAllColliders()
+    {
+        Physics2D.alwaysShowColliders = !Physics2D.alwaysShowColliders;
+    }
 
     [MenuItem("SG7/Build/Build Windows %w")]
     public static void buildWindows()
