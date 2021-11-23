047005a1d948787248abbe8d3156fd981fe5f9d4
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-23 06:37:13 -0500

    Tall Grass reduces player visibility

---
+++ Assets/Scripts/TallGrass.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TallGrass : MonoBehaviour
+{
+    [Range(0,1)]
+    [Tooltip("The alpha value of player sprites when inside the tall grass")]
+    public float hiddenAlpha = 0.5f;
+
+    private Collider2D coll2d;
+
+    private void Start()
+    {
+        coll2d = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        checkSetAlpha(collision.gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        checkSetAlpha(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        checkSetAlpha(collision.gameObject);
+    }
+
+    void checkSetAlpha(GameObject go)
+    {
+        PlayerController pc = go.GetComponent<PlayerController>();
+        if (pc)
+        {
+            if (coll2d.OverlapPoint(go.transform.position))
+            {
+                setAlpha(go, hiddenAlpha);
+            }
+            else
+            {
+                setAlpha(go, 1);
+            }
+        }
+    }
+
+    void setAlpha(GameObject go, float alpha)
+    {
+        SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
+        Color color = sr.color;
+        color.a = alpha;
+        sr.color = color;
+    }
+}
