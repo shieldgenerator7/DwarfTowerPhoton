e5aebb249aa0c96575cd56047fccfdd4dcce07e0
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-22 19:36:13 -0500

    Add DashAbility.activate()

--- Assets/Characters/Astral/Abilities/DashAbility.cs
+++ Assets/Characters/Astral/Abilities/DashAbility.cs
@@ -14,22 +14,31 @@ public class DashAbility : PlayerAbility
         base.OnButtonDown();
         
         if(playerController.requestAmina(manaCost) > 0){
-            Vector2 moveDir = (Utility.MouseWorldPos - transform.position).normalized;
-            //Give enough force for rb2d to move character entire distance in single frame
-            rb2d.velocity = moveDir * dashDistance / Time.deltaTime;
-            zeroVelocity = true;
-            playerMovement.enabled = false;
+            activate();
         }
     }
     public override void OnButtonHeld()
     {
         base.OnButtonHeld();
+
+        deactivate();     
+    }
+
+    void activate()
+    {
+        Vector2 moveDir = (Utility.MouseWorldPos - transform.position).normalized;
+        //Give enough force for rb2d to move character entire distance in single frame
+        rb2d.velocity = moveDir * dashDistance / Time.deltaTime;
+        zeroVelocity = true;
+        playerMovement.enabled = false;
+    }
+    void deactivate()
+    {
         if (zeroVelocity)
         {
             zeroVelocity = false;
             rb2d.velocity = Vector2.zero;
             playerMovement.enabled = true;
         }
-        
     }
 }
