bc74ea084c3f328aea708448ff9d15943ad792c7
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-23 02:42:03 -0500

    Add OnDieDestroy

---
+++ Assets/Scripts/OnDieDestroy.cs
@@ -0,0 +1,28 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HealthPool))]
+public class OnDieDestroy : MonoBehaviour
+{
+    private PhotonView PV;
+    private HealthPool healthPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        healthPool.onDied += () =>
+        {
+            if (PV.IsMine)
+            {
+                PhotonNetwork.Destroy(this.gameObject);
+            }
+            else
+            {
+                GetComponent<Collider2D>().enabled = false;
+            }
+        };
+    }
+}
