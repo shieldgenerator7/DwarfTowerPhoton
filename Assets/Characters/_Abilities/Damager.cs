96af6212ea3658bbaefb3798ec0baca4fc48c774
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-27 15:08:16 -0500

    Add Damager

---
+++ Assets/Characters/_Abilities/Damager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    [Tooltip("How much damage to deal")]
+    public float damage;
+
+    [Tooltip("The EntityTypes that this damager can damage")]
+    public List<EntityType> damagableTypes;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        processCollision(collision, true);
+    }
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    processCollision(collision, false);
+    //}
+
+    protected virtual void processCollision(Collider2D collision, bool useInitialDamage)
+    {
+        if (TeamToken.onSameTeam(gameObject, collision.gameObject))
+        {
+            //don't damage teammates
+            return;
+        }
+        HealthPool hp = collision.gameObject.GetComponent<HealthPool>();
+        if (hp)
+        {
+            if (damagableTypes.Contains(hp.entityType))
+            {
+                if (useInitialDamage)
+                {
+                    hp.Health += -damage;
+                }
+            }
+        }
+    }
+}
