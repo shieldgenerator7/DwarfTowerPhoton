8f0320c6f05de61392e8f7accf3f6726f91acbd3
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-28 16:31:02 -0500

    Fix bug: IOOBE when character's default color isn't in the list

--- Assets/Scripts/Photon/PlayerInfo.cs
+++ Assets/Scripts/Photon/PlayerInfo.cs
@@ -84,6 +84,18 @@ public class PlayerInfo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Check default colors
+        foreach (CharacterInfo charInfo in allCharacters)
+        {
+            if (!allColors.Contains(charInfo.defaultColor))
+            {
+                allColors.Add(charInfo.defaultColor);
+                Debug.LogError(
+                    "defaultColor not in the list! adding color: "
+                    + ColorUtility.ToHtmlStringRGB(charInfo.defaultColor)
+                    );
+            }
+        }
         //if (PlayerPrefs.HasKey("MyCharacter"))
         //{
         //    mySelectedCharacter = PlayerPrefs.GetInt("MyCharacter");
