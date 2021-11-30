32c8c7d866f106b4859ecdb273ecfbcddec908d8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-11-30 07:28:56 -0500

    [SNIPER] Laser changes alpha

--- Assets/Characters/Sniper/Abilities/LaserShotController.cs
+++ Assets/Characters/Sniper/Abilities/LaserShotController.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class LaserShotController : ShotController
 {
+    [Range(0, 2)]
     [Tooltip("The initial width it is when it closes in")]
     public float startWidth = 1;
+    [Range(0, 2)]
     [Tooltip("The final width it is when it deals damage")]
     public float endWidth = 0.1f;
+    [Range(0, 1)]
+    [Tooltip("The initial alpha it is when it closes in")]
+    public float startAlpha = 0.1f;
+    [Range(0, 1)]
+    [Tooltip("The final alpha it is when it deals damage")]
+    public float endAlpha = 1;
+    [Range(0, 2)]
     [Tooltip("The amount of seconds to deal damage after releasing")]
     public float closeInDuration = 0.5f;
+    [Range(0, 2)]
     [Tooltip("The amount of seconds it stays after starting to deal damage")]
     public float stayDuration = 0.2f;
     [Tooltip("How far the laser goes")]
@@ -73,21 +83,27 @@ public class LaserShotController : ShotController
     private void updateWidth()
     {
         float percent = Mathf.Clamp(
-            1-(((fireStartTime + closeInDuration) - Time.time) / closeInDuration),
+            1 - (((fireStartTime + closeInDuration) - Time.time) / closeInDuration),
             0,
             1
             );
-        float diff = ((endWidth - startWidth) * percent);
-        float width = diff + startWidth;
-        Debug.Log("percent: " + percent + ", diff: " + diff + ", width: " + width);
+        float widthDiff = ((endWidth - startWidth) * percent);
+        float width = widthDiff + startWidth;
         Vector3 scale = transform.localScale;
         scale.x = width;
         transform.localScale = scale;
+        //Sprite transparency
+        float alphaDiff = ((endAlpha - startAlpha) * percent);
+        float alpha = alphaDiff + startAlpha;
+        Color color = sr.color;
+        color.a = alpha;
+        sr.color = color;
     }
 
     private float fireStartTime = -1;
 
     private Collider2D coll2d;
+    private SpriteRenderer sr;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -95,10 +111,15 @@ public class LaserShotController : ShotController
         base.Start();
         coll2d = gameObject.FindComponent<Collider2D>();
         coll2d.enabled = false;
+        sr = gameObject.FindComponent<SpriteRenderer>();
         //Start Width
         Vector3 scale = transform.localScale;
         scale.x = startWidth;
         transform.localScale = scale;
+        //Start alpha
+        Color color = sr.color;
+        color.a = startAlpha;
+        sr.color = color;
     }
 
     // Update is called once per frame
