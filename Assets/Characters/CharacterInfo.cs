d9b6275304cc2ecc87a61a9e898cf138005f0435
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-12-01 23:38:10 -0500

    #37 Add character difficulty rating

--- Assets/Characters/CharacterInfo.cs
+++ Assets/Characters/CharacterInfo.cs
@@ -9,6 +9,10 @@ public class CharacterInfo : ScriptableObject
     public Sprite sprite;
     public Color defaultColor = Color.white;
     public GameObject prefab;
+    [Range(1,3)]
+    [Tooltip("How difficult this character is perceived to be")]
+    public int difficulty = 1;//the more stars, the more difficult
+    [Tooltip("Roles that this character best fits into")]
     public List<CharacterType> typeList;
 
     public string typeString
