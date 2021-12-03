d1b6740cfacd4279ed585e5ede7bd4b9938aa854
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-12-03 18:42:44 -0500

    Rename Input -> InputState

--- Assets/Scripts/InputState.cs
+++ Assets/Scripts/InputState.cs
@@ -1,5 +1,51 @@
 
 
+using UnityEngine;
 
+public struct InputState
 {
+    public Vector2 movement;
+    public ButtonState ability0;//reload
+    public ButtonState ability1;//LMB
+    public ButtonState ability2;//RMB
+    public ButtonState ability3;//space
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is InputState))
+        {
+            return false;
+        }
+        return this == ((InputState)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        //2021-12-03: copied from https://stackoverflow.com/a/1646913/2336212
+        int hash = 17;
+        hash = hash * 31 + movement.x.GetHashCode();
+        hash = hash * 31 + movement.y.GetHashCode();
+        hash = hash * 31 + ability0.GetHashCode();
+        hash = hash * 31 + ability1.GetHashCode();
+        hash = hash * 31 + ability2.GetHashCode();
+        hash = hash * 31 + ability3.GetHashCode();
+        return hash;
+    }
+
+    public static bool operator ==(InputState a, InputState b)
+    {
+        return a.movement == b.movement
+            && a.ability0 == b.ability0
+            && a.ability1 == b.ability1
+            && a.ability2 == b.ability2
+            && a.ability3 == b.ability3;
+    }
+    public static bool operator !=(InputState a, InputState b)
+    {
+        return a.movement != b.movement
+            || a.ability0 != b.ability0
+            || a.ability1 != b.ability1
+            || a.ability2 != b.ability2
+            || a.ability3 != b.ability3;
+    }
 }
