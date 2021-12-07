d350634de70bc5781e36eb410c9e60bb1d5cbee0
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-12-07 13:07:36 -0500

    Prevent overlapping parallel path segments in Point Collection algorithm

--- Assets/Scripts/PointCollectionPathAlgorithm.cs
+++ Assets/Scripts/PointCollectionPathAlgorithm.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PointCollectionAlgorithm", menuName = "Map Generation/Path/Point Collection", order = 1)]
 public class PointCollectionPathAlgorithm : MapPathGeneratorAlgorithm
 {
     [Range(0, 10)]
+    [Tooltip("How many points to collect. The number of points will be ((pointCount * 2) + 2)")]
     public int pointCount = 5;
-    public float buffer = 5;
+    [Range(0, 10)]
+    [Tooltip("Points should have x and y values that differ by at least this amount. " +
+        "This is to prevent overlapping parallel path segments")]
+    public float minPointDiff = 1;
+    [Range(0, 10)]
+    [Tooltip("The y buffer to apply to the area between the startPos and the middle")]
+    public float bufferY = 5;
+    [Tooltip("Points that generated points should not spawn aligned with, either horizontally or vertically")]
+    public List<Vector2> avoidPointList;
 
     public override MapPath generate(PathGenerationRequirements pgp)
     {
@@ -15,20 +25,40 @@ public class PointCollectionPathAlgorithm : MapPathGeneratorAlgorithm
         Bounds bounds = pgp.bounds;
         bounds.min = new Vector2(
             bounds.min.x,
-            pgp.startPos.y + buffer
+            pgp.startPos.y + bufferY
             );
         bounds.max = new Vector2(
             bounds.max.x,
-            pgp.middle.y - buffer
+            pgp.middle.y - bufferY
             );
         //Generate points to "collect"
         List<Vector2> pointList = new List<Vector2>();
         for (int i = 0; i < pointCount; i++)
         {
-            Vector2 point = new Vector2(
-                Random.Range(bounds.min.x, bounds.max.x),
-                Random.Range(bounds.min.y, bounds.max.y)
-                );
+            int safetyEject = 100;
+            Vector2 point;
+            do
+            {
+                point = new Vector2(
+                    Random.Range(bounds.min.x, bounds.max.x),
+                    Random.Range(bounds.min.y, bounds.max.y)
+                    );
+                safetyEject--;
+                if (safetyEject == 0)
+                {
+                    Debug.Log($"Safety eject! point: {point}");
+                    break;
+                }
+            }
+            while (pointList.Any(
+                p => Mathf.Abs(p.x - point.x) < minPointDiff || Mathf.Abs(p.y - point.y) < minPointDiff
+                )
+            || avoidPointList.Any(
+                p => Mathf.Abs(p.x - point.x) < minPointDiff || Mathf.Abs(p.y - point.y) < minPointDiff
+                )
+            );
+            //Point is going to cause overlapping parallel path segments, so add it
+            //(or safety eject was used, in which case add it anyway)
             pointList.Add(point);
         }
         //Add start position to points to collect
