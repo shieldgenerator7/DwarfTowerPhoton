e957cbc4999dd0a094a16c2129f24bd79823e630
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-12-14 03:18:29 -0500

    Tool: AutoSelectCharacter

---
+++ Assets/Scripts/Tools/AutoSelectCharacter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSelectCharacter : MonoBehaviour
+{
+    [Range(0, 3)]
+    public int characterIndex;
+    [Range(0, 5)]
+    public int colorIndex;
+    public string mapName;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Do once before
+        AutoSelect();
+    }
+    private void Start()
+    {
+        //And do once after
+        AutoSelect();
+    }
+    private void AutoSelect()
+    {
+        PlayerInfo info = FindObjectOfType<PlayerInfo>();
+        info.SelectedIndex = characterIndex;
+        info.ColorIndex = colorIndex;
+        info.mapName = mapName;
+    }
+}
