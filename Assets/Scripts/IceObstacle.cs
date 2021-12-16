06bde7846c11a50101c32b62e998d687f5395e29
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-12-15 21:18:57 -0500

    #50 Fix bug: getting stuck on other obstacles while using ice puddle

--- Assets/Scripts/IceObstacle.cs
+++ Assets/Scripts/IceObstacle.cs
@@ -26,9 +26,20 @@ public class IceObstacle : MonoBehaviour
         PlayerMovement playerMovement = collision.gameObject.FindComponent<PlayerMovement>();
         if (playerMovement && playerMovement.isPhotonViewMine())
         {
-            playerMovement.forceMovement(playerMovement.LastMoveDirection);
-            StatKeeper statKeeper = playerMovement.gameObject.FindComponent<StatKeeper>();
-            statKeeper.selfStats.addLayer(PV.ViewID, slipLayer);
+            slipPlayer(playerMovement, true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerMovement playerMovement = collision.gameObject.FindComponent<PlayerMovement>();
+        if (playerMovement && playerMovement.isPhotonViewMine())
+        {
+            slipPlayer(playerMovement, true);
+            if (Mathf.Approximately(0, playerMovement.rb2d.velocity.magnitude))
+            {
+                slipPlayer(playerMovement, false);
+            }
         }
     }
 
@@ -37,8 +48,21 @@ public class IceObstacle : MonoBehaviour
         PlayerMovement playerMovement = collision.gameObject.FindComponent<PlayerMovement>();
         if (playerMovement && playerMovement.isPhotonViewMine())
         {
+            slipPlayer(playerMovement, false);
+        }
+    }
+
+    void slipPlayer(PlayerMovement playerMovement, bool slip)
+    {
+        StatKeeper statKeeper = playerMovement.gameObject.FindComponent<StatKeeper>();
+        if (slip)
+        {
+            playerMovement.forceMovement(playerMovement.LastMoveDirection);
+            statKeeper.selfStats.addLayer(PV.ViewID, slipLayer);
+        }
+        else
+        {
             playerMovement.forceMovement(false);
-            StatKeeper statKeeper = playerMovement.gameObject.FindComponent<StatKeeper>();
             statKeeper.selfStats.removeLayer(PV.ViewID);
         }
     }
