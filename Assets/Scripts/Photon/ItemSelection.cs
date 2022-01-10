b2d69a8f3b29008bc711db2e981175db27ef5a41
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-01-09 23:28:49 -0500

    #64 Add ItemSelection

---
+++ Assets/Scripts/Photon/ItemSelection.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemSelection<T>
+{
+    [SerializeField]
+    private int index = -1;
+    public int Index
+    {
+        get => index;
+        set
+        {
+            index = Mathf.Clamp(value, -1, itemList.Count);
+            onIndexChanged?.Invoke(index);
+        }
+    }
+    public delegate void OnIndexChanged(int index);
+    public event OnIndexChanged onIndexChanged;
+
+    public T SelectedItem
+    {
+        get => (index >= 0)
+            ? itemList[index]
+            : itemList[0];
+        set
+        {
+            Index = itemList.IndexOf(value);
+        }
+    }
+    public List<T> itemList;
+
+    public T this[int i]
+    {
+        get => itemList[i];
+        set => itemList[i] = value;
+    }
+
+    public bool Contains(T item) => itemList.Contains(item);
+
+    public int IndexOf(T item) => itemList.IndexOf(item);
+}
