5e09e952a3ae8c18e2566db8c7cceb647bed24c3
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-04 06:41:02 -0500

    #70 TAB screen

---
+++ Assets/Scripts/TabScreenDisplay.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabScreenDisplay : MenuDisplay
+{
+    public GameObject playerIconPrefab;
+    public GameObject separatorPrefab;
+    public KeyCode activateKeyCode;
+    public int redColorGroupIndex = 0;
+    public int blueColorGroupIndex = 1;
+    [Range(10,20)]
+    public int maxItemsInList = 11;
+
+    private List<GameObject> playerIcons = new List<GameObject>();
+
+    private RectTransform rect;
+    private RectTransform rectParent;
+    private GridLayoutGroup glg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = gameObject.GetComponent<RectTransform>();
+        rectParent = rect.parent.GetComponent<RectTransform>();
+        glg = gameObject.FindComponent<GridLayoutGroup>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(activateKeyCode))
+        {
+            Display(true);
+        }
+        else if (Input.GetKeyUp(activateKeyCode))
+        {
+            Display(false);
+        }
+    }
+
+    public void Display(bool display)
+    {
+        //Destroy all current icons
+        glg.transform.DetachChildren();
+        playerIcons.ForEach(pi => Destroy(pi));
+        playerIcons.Clear();
+        //Create new icons
+        if (display)
+        {
+            List<TeamTokenCaptain> ttcList = FindObjectsOfType<TeamTokenCaptain>().ToList();
+            List<PlayerController> pcList = FindObjectsOfType<PlayerController>().ToList();
+            //Red team
+            TeamTokenCaptain ttcRed = ttcList.Find(
+                ttc => ttc.colorGroupIndex == redColorGroupIndex
+                );
+            pcList.FindAll(
+                pc => ttcRed.onSameTeam(TeamToken.getTeamToken(pc.gameObject))
+                )
+                .ForEach(pc => playerIcons.Add(CreatePlayerIcon(pc)));
+            //Separator
+            playerIcons.Add(Instantiate(separatorPrefab));
+            //Blue team
+            TeamTokenCaptain ttcBlue = ttcList.Find(
+                ttc => ttc.colorGroupIndex == blueColorGroupIndex
+                );
+            pcList.FindAll(
+                pc => ttcBlue.onSameTeam(TeamToken.getTeamToken(pc.gameObject))
+                )
+                .ForEach(pc => playerIcons.Add(CreatePlayerIcon(pc)));
+            //Layout icons
+            playerIcons.ForEach(pi => pi.transform.parent = glg.transform);
+        }
+    }
+
+    private GameObject CreatePlayerIcon(PlayerController playerController)
+    {
+        GameObject playerIcon = Instantiate(playerIconPrefab);
+        Image image = playerIcon.FindComponent<Image>();
+        image.color = playerController.playerColor;
+        image.sprite = playerController.characterInfo.sprite;
+        return playerIcon;
+    }
+}
