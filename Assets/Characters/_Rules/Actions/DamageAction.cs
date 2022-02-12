ff793556295d9e533da9b061c73c5c47ce929245
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-12 05:05:09 -0500

    Turn RuleAction into an enum

--- Assets/Characters/_Rules/Actions/DamageAction.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DamageAction", menuName = "Characters/Rule/Action/DamageAction", order = 0)]
-public class DamageAction : RuleAction
-{
-    
-}
--- Assets/Characters/_Rules/RuleAction.cs
+++ Assets/Characters/_Rules/RuleAction.cs
@@ -1,8 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-//[CreateAssetMenu(fileName = "RuleAction", menuName = "Characters/Rule/RuleAction", order = 0)]
-public abstract class RuleAction : ScriptableObject
+
+
+
+
+public enum RuleAction
 {
+    MOVE_IN_TARGET_DIR,
+    MOVE_TOWARDS_TARGET_POS,
+    DAMAGE,
 }
