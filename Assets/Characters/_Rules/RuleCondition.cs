9b737bb92e30267f6f35b9cec18b28c3da4a1e80
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-13 03:59:00 -0500

    Extract TargetCondition from RuleCondition

--- Assets/Characters/_Rules/RuleCondition.cs
+++ Assets/Characters/_Rules/RuleCondition.cs
@@ -2,13 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "RuleCondition", menuName = "Characters/Rule/RuleCondition", order = 0)]
-public class RuleCondition: ScriptableObject
+//[CreateAssetMenu(fileName = "RuleCondition", menuName = "Characters/Rule/RuleCondition", order = 0)]
+public abstract class RuleCondition : ScriptableObject
 {
-    public List<EntityType> entityTypes;
-    public bool onAlly = true;
-    public bool onEnemy = true;
-    public bool onTrigger = true;
-    public bool onCollision = true; 
     public abstract bool Check(RuleContext context);
 }
---
+++ Assets/Characters/_Rules/TargetCondition.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TargetCondition", menuName = "Characters/Rule/TargetCondition", order = 0)]
+public class TargetCondition : RuleCondition
+{
+    public List<EntityType> entityTypes;
+    public bool onAlly = true;
+    public bool onEnemy = true;
+    public bool onTrigger = true;
+    public bool onCollision = true;
+
+    public override bool Check(RuleContext context)
+    {
+        bool onSameTeam = TeamToken.onSameTeam(context.self, context.target);
+        if (onAlly && onSameTeam || onEnemy && !onSameTeam)
+        {
+            if (onTrigger && context.isTrigger || onCollision && context.isCollision)
+            {
+                HealthPool hp = context.target.FindComponent<HealthPool>();
+                if (hp)
+                {
+                    if (entityTypes.Contains(hp.entityType))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
