1a94d15a8513e9e7fbf1c8b6d28e3f7252b1b5c8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-15 03:35:24 -0500

    Add InputButton

---
+++ Assets/Scripts/InputButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputButton
+{
+    Ability1,
+    Ability2,
+    Ability3,
+    Reload,
+    MoveTowardsCursor,
+}
