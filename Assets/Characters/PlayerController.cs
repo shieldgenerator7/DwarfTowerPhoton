a08b24a3194ada40899458ab9ba273a3f7833693
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-16 02:51:58 -0500

    Make PlayerController abstract again

--- Assets/Characters/PlayerController.cs
+++ Assets/Characters/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public abstract class PlayerController : MonoBehaviour
 {
     public CharacterInfo characterInfo;
 
