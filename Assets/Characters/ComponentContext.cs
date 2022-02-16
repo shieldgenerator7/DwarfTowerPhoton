727b22ac169d20a3dd792cf0415554c6176e09ba
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-15 23:24:57 -0500

    Improve ComponentContext

--- Assets/Characters/ComponentContext.cs
+++ Assets/Characters/ComponentContext.cs
@@ -8,18 +8,19 @@ using UnityEngine;
 /// </summary>
 public class ComponentContext : MonoBehaviour
 {
-    public AminaReloader aminaReloader;//ability called by default when the player runs out of amina
+    public AminaReloader aminaReloader { get; private set; }
     public PhotonView PV { get; private set; }
     public AminaPool aminaPool { get; private set; }
     public HealthPool healthPool { get; private set; }
     public Damager damager { get; private set; }
     public PlayerInput playerInput { get; private set; }
+    public PlayerController playerController { get; private set; }
     public PlayerMovement playerMovement { get; private set; }
     public StatKeeper statKeeper { get; private set; }
     public StatusKeeper statusKeeper { get; private set; }
+    public StatusAutoEnder statusAutoEnder { get; private set; }
     public ObjectSpawner objectSpawner { get; private set; }
     public SpriteRenderer sr { get; private set; }
-    public StatusAutoEnder statusAutoEnder { get; private set; }
     public TeamToken teamToken { get; private set; }
 
     // Start is called before the first frame update
@@ -31,17 +32,21 @@ public class ComponentContext : MonoBehaviour
     {
         PV = gameObject.FindComponent<PhotonView>();
         playerInput = gameObject.FindComponent<PlayerInput>();
+        playerController = gameObject.FindComponent<PlayerController>();
         playerMovement = gameObject.FindComponent<PlayerMovement>();
         playerMovement.Start();
         sr = gameObject.FindComponent<SpriteRenderer>();
         damager = gameObject.FindComponent<Damager>();
         healthPool = gameObject.FindComponent<HealthPool>();
         aminaPool = gameObject.FindComponent<AminaPool>();
+        objectSpawner = gameObject.FindComponent<ObjectSpawner>();
         statKeeper = gameObject.FindComponent<StatKeeper>();
         statusKeeper = gameObject.FindComponent<StatusKeeper>();
-        objectSpawner = gameObject.FindComponent<ObjectSpawner>();
-        statusAutoEnder = gameObject.FindComponent<StatusAutoEnder>();
-        statusAutoEnder.Init(statusKeeper);
+        if (statusKeeper)
+        {
+            statusAutoEnder = gameObject.FindComponent<StatusAutoEnder>();
+            statusAutoEnder?.Init(statusKeeper);
+        }
         teamToken = gameObject.FindComponent<TeamToken>();
     }
 }
