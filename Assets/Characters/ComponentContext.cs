e38bbd1bb63b045615e8060800318d1110c88194
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-15 23:35:42 -0500

    Update character prefabs with ComponentContext

--- Assets/Characters/ComponentContext.cs
+++ Assets/Characters/ComponentContext.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 /// <summary>
 /// Stores references to other components
 /// </summary>
