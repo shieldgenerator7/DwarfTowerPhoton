66048e4c28fa794ae48846eec7fd4ab70d6ccb16
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-23 02:03:22 -0500

    Add stat and status layers to RuleSettings

--- Assets/Characters/_Rules/RuleSettings.cs
+++ Assets/Characters/_Rules/RuleSettings.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 [System.Serializable]
 public struct RuleSettings
 {
-    public List<RuleSetting> settings;
-
     public RuleSet targetRuleSet;
+    //public List<ObjectSpawnInfo> objectsToSpawn;
+    public StatLayer statLayer;
+    public StatusLayer statusLayer;
+
+    public List<RuleSetting> settings;
 
     public RuleSetting Get(RuleSetting.Option setting)
         => settings.Find(s => s.setting == setting);
