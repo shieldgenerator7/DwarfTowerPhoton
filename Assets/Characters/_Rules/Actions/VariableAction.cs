770bcff19f17ae01e2dfbcef7d04cf2ba7a1debe
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-02-28 00:08:38 -0500

    Make VariableAction more general (for potentially more variables)

--- Assets/Characters/_Rules/Actions/VariableAction.cs
+++ Assets/Characters/_Rules/Actions/VariableAction.cs
@@ -2,22 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "MultiplierAction", menuName = "Rule/Action/MultiplierAction", order = 0)]
+[CreateAssetMenu(fileName = "VariableAction", menuName = "Rule/Action/VariableAction", order = 0)]
 public class VariableAction : RuleAction
 {
-    public enum Action
+    public enum Variable
     {
-        SET_STAT_MULTIPLIER_FROM_RESERVED_AMINA,
-        RESET_STAT_MULTIPLIER,
+        MULTIPLIER,
+        TARGET_DIR,
+        TARGET_POS,
     }
-    public Action action;
+    public Variable variable;
+    public enum Source
+    {
+        RESET,
+        RESERVED_AMINA,
+    }
+    public Source source;
 
     public override void TakeAction(RuleSettings settings, ref RuleContext context)
     {
+        Vector2 sourceVector = Vector2.zero;
+        float sourceFloat = 0;
         ComponentContext compContext = context.componentContext;
-        switch (action)
+        switch (source)
         {
-            case Action.SET_STAT_MULTIPLIER_FROM_RESERVED_AMINA:
+            case Source.RESET:
+                //TODO: make specific resets for each variable (need separate if statement)
+                sourceFloat = 1;
+                sourceVector = Vector2.zero;
+                break;
+            case Source.RESERVED_AMINA:
                 float reservedAmina = compContext.aminaPool.ReservedAmina;
                 float minAmina = settings.Get(RuleSetting.Option.AMINA_COST);
                 float factor = reservedAmina / minAmina;
@@ -26,13 +40,24 @@ public class VariableAction : RuleAction
                 {
                     factor = Mathf.Min(factor, maxFactor.Value);
                 }
-                context.statMultiplier = factor;
-                break;
-            case Action.RESET_STAT_MULTIPLIER:
-                context.statMultiplier = 1;
+                sourceFloat = factor;
                 break;
             default:
-                throw new System.ArgumentException($"Unknown action enum: {action}");
+                throw new System.ArgumentException($"Unknown source enum: {source}");
+        }
+        switch (variable)
+        {
+            case Variable.MULTIPLIER:
+                context.statMultiplier = sourceFloat;
+                break;
+            case Variable.TARGET_DIR:
+                context.targetDir = sourceVector;
+                break;
+            case Variable.TARGET_POS:
+                context.targetPos = sourceVector;
+                break;
+            default:
+                throw new System.ArgumentException($"Unknown variable enum: {variable}");
         }
     }
 }
