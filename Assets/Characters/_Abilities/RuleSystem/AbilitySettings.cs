8a50e3ab07bad4eb40f6d165cd2b8f6deae6c231
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-10-07 18:44:53 -0400

    Add two attributes to AbilitySettings

--- Assets/Characters/_Abilities/RuleSystem/AbilitySettings.cs
+++ Assets/Characters/_Abilities/RuleSystem/AbilitySettings.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Ability", menuName = "RuleSystem/AbilitySettings")]
 public class AbilitySettings : ScriptableObject
 {
-
+    public new string name;
+    public int id;
 }
